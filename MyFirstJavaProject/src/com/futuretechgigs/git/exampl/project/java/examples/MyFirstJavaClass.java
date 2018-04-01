68ddc615741430c0350381e1f35b59882a2d9bf7
rckstrjanu <rckstrjanu@gmail.com>
2018-04-01 13:49:13 +0530

    Committing my first Git hub file

---
+++ MyFirstJavaProject/src/com/futuretechgigs/git/exampl/project/java/examples/MyFirstJavaClass.java
@@ -0,0 +1,22 @@
+package com.futuretechgigs.git.exampl.project.java.examples;
+
+/**
+ * Class which peforms the print operation.
+ * 
+ * @author futuretechgigs
+ *
+ */
+public class MyFirstJavaClass {
+
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the FutureTechgigs Git Configuration tutorial.");
+
+	}
+
+}
